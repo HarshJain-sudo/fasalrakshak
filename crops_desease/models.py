5699796aaba81f471363caa313d6a6cdc842fd30
hj60490 <harshj60490@gmail.com>
2023-03-22 13:17:54 +0530

    add models

--- crops_desease/models.py
+++ crops_desease/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Vegetable(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    symptoms = models.ManyToManyField('Symptom')
+    precautions = models.ManyToManyField('Precaution')
+
+    def __str__(self):
+        return self.name
+
+
+class Fruit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    symptoms = models.ManyToManyField('Symptom')
+    precautions = models.ManyToManyField('Precaution')
+
+    def __str__(self):
+        return self.name
+
+
+class Symptom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Precaution(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
